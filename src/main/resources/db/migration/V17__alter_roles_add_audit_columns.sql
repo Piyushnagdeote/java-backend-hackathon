62330db87d2f1e2403ee8f3c0f79f6e7ed38ba02
Piyushnagdeote <pnagdeote219@gmail.com>
2026-01-23 17:15:47 +0530

    chore: remove invalid Flyway migration V17

--- src/main/resources/db/migration/V17__alter_roles_add_audit_columns.sql
+++
@@ -1,8 +0,0 @@
--- Fix roles table to match BaseEntity
-
-ALTER TABLE roles
-    ADD COLUMN created_at DATETIME NULL,
-    ADD COLUMN updated_at DATETIME NULL,
-    ADD COLUMN created_by VARCHAR(255) NULL,
-    ADD COLUMN deleted BIT(1) NOT NULL DEFAULT 0,
-    ADD COLUMN version BIGINT DEFAULT 0;
